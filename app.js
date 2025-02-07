d06b09795e0ecb5f15d41983967eb82fe5f73df0
Sebastian Murillo <sebastianmurillo63@gmail.com>
2025-02-07 07:51:49 -0500

    funcion agregarAmigo() y sortearAmigo() agregadas

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let arrayNombres = [];
 
-function agregarAmigo(nombre){
+function agregarAmigo(){
+    let nombre = document.getElementById("amigo").value;
+    
+    //Si no se ingresa ningun dato
     if (!nombre || nombre.trim() === "") {
 
-        alert("Debes ingresar un dato")
-
+        alert("Debes ingresar un dato");
+    
+    //Si la informacion ingresada es un entero
     } else if(!isNaN(nombre)){
 
-        alert("Los datos ingresados no pueden ser valores numericos")
+        alert("Los datos ingresados no pueden ser valores numericos");
         
     } else {
-        arrayNombres.push(prompt(nombre));
+        arrayNombres.push(nombre);
+        console.log(arrayNombres);
     }
     
 }
