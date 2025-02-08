c582d7aac7e3f4adcf854c014347033068d05421
sebs-001 <sebastianmurillo63@gmail.com>
2025-02-08 18:29:47 -0500

    Se agrego id al ul de la linea 23

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let arrayNombres = [];
 
+//Se encarga de agregar nombres a el array "arrayNombres"
 function agregarAmigo(){
+    let listaAmigos = document.getElementById("listaAmigos");
     let nombre = document.getElementById("amigo").value;
-    
     //Si no se ingresa ningun dato en la caja
     if (!nombre || nombre.trim() === "") {
 
@@ -17,12 +18,33 @@ function agregarAmigo(){
     } else {
         arrayNombres.push(nombre);
         console.log(arrayNombres);
+        listaAmigos.innerHTML += nombre + "<br>";
     }
     limpiarCaja();
-    
 }
 
+
+
+//Se encarga de seleccionar un indice aleatorio del array en el cual esta almacenado un nombre
 function sortearAmigo(){
+
+    //Si la lista esta vacia
+    if (!arrayNombres || arrayNombres.length === 0) {
+
+        alert("No hay nombres en la lista")
+        return;
+
+    } else {
+        let selectorLista = document.getElementById("listaAmigos");
+        let nombreSorteado = document.getElementById("resultado");
+        let indiceNombre = Math.floor(Math.random() * arrayNombres.length);
+        let nombreSeleccionado = arrayNombres[indiceNombre];
+
+        nombreSorteado.innerHTML = nombreSeleccionado;
+        selectorLista.innerHTML = "";
+        document.getElementById("agregarPana").setAttribute("disabled", "true");
+        return arrayNombres[indiceNombre];
+    }
     
 }
 
@@ -31,3 +53,4 @@ function limpiarCaja(){
     let valorCaja = document.querySelector("#amigo");
     valorCaja.value = "";
 }
+
